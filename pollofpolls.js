437fade30c21c2b1d5c7216266abe6341a79516d
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-08-29 11:05:23 +0200

    Funker med cache

--- pollofpolls.js
+++ pollofpolls.js
@@ -4,6 +4,8 @@ fetch('http://www.pollofpolls.no/api/gallup.php', { method: "post" })
   .then(response => response.text())
   .then(data => {
     data = data.replace(/(?:\r\n|\r|\n)/g, ',').slice(0,-1)
-    output = `[${data}]`
-    fs.writeFileSync('./polls.json', output)
+    const json = JSON.parse(`[${data}]`)
+    const newPolls = json.filter(p => p.dato.startsWith("2023-07") || p.dato.startsWith("2023-08"))
+    const localPolls = newPolls.filter(p => p.type == "kommune" && p.kommuneid != "0")
+    fs.writeFileSync('./polls.json', localPolls)
   })
